58072b85d0a34e8dc66b9193991ed88b4160a100
chaosman97 <genious.kallias@gmail.com>
2023-04-09 00:19:37 +0300

    gemisma pinaka

--- ergasia.c
+++ ergasia.c
@@ -15,7 +15,7 @@ int lc(FILE* ptr); //orismos synartisis line counter
 
 int main(){
     team *t;
-    int N;
+    int N,i=0;
     char filename[50];
     FILE* file;
     printf("Doste to onoma toy arxeioy: ");//eisagogi arxeiou
@@ -31,14 +31,18 @@ int main(){
         printf("Problhma stin ekxorisi pinaka");
         return 0;
     }
+    while(i!=N){
+        fscanf(file,"%s %s %d %d",t[i].name,t[i].country,t[i].D_entos,t[i].D_ektos);
+        i++;    //gemisma pinaka domon
+    }
     free(t);
     return 0;
 }
- int lc(FILE* ptr){
+int lc(FILE* ptr){
     int counter=0,den,dek;
     char country[50],name[50];
     for(;!feof(ptr);counter++){
         fscanf(ptr,"%s %s %d %d",name,country,&den,&dek);
     }
     return counter;
- }
+}
