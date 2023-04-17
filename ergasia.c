44fba95410ed1965729a5531b0a1e1b1c333df3d
chaosman97 <genious.kallias@gmail.com>
2023-04-17 20:26:51 +0300

    first round halfway done

--- ergasia.c
+++ ergasia.c
@@ -14,9 +14,11 @@ typedef struct{
     char country[50];
 }team;
 
-int lc(FILE* ptr); //orisma synartisis line counter
-int pv(int x); //orisma synartisis psychology victory
-int pl(int y); //orisma synartisis psychology loss
+int lc(FILE* ptr); //synartisi line counter
+int pv(int x); //synartisi psychology victory
+int pl(int y); //synartisi psychology loss
+void score(int *z,int *w); //synartisi score
+void bubble_sort(team *x,int n); //synartisi taksinomisis
 int main(){
     team *t;
     int N,i=0,j,s1,s2,s;
@@ -58,6 +60,7 @@ int main(){
                         (t+j)->points++;
                         (t+i)->active_score+=s1;
                         (t+j)->passive_score+=s1;
+                        score(&(t+i)->points,&(t+j)->points);
                     }
                     else if(s1<s2){
                         (t+j)->P=pv((t+j)->P);
@@ -66,16 +69,19 @@ int main(){
                         (t+i)->points++;
                         (t+j)->active_score+=s2;
                         (t+i)->passive_score+=s2;
+                        score(&(t+j)->points,&(t+i)->points);
                     }
                     else{
                         s=rand()%2;
                         if(s==1){
                             (t+j)->P=pv((t+j)->P);
                             (t+i)->P=pl((t+i)->P);
+                            (t+j)->points++;
                         }
                         else{
                             (t+i)->P=pv((t+i)->P);
                             (t+j)->P=pl((t+j)->P);
+                            (t+i)->points++;
                         }
                     }
                 }
@@ -85,25 +91,37 @@ int main(){
                     if(s1>s2){
                         (t+i)->P=pv((t+i)->P);
                         (t+j)->P=pl((t+j)->P);
+                        score(&(t+i)->points,&(t+j)->points);
                     }
                     else if(s1<s2){
                         (t+j)->P=pv((t+j)->P);
                         (t+i)->P=pl((t+i)->P);
+                        score(&(t+j)->points,&(t+i)->points);
                     }
                     else{
                         s=rand()%2;
                         if(s==1){
                             (t+j)->P=pv((t+j)->P);
                             (t+i)->P=pl((t+i)->P);
+                            (t+j)->points++;
                         }
                         else{
                             (t+i)->P=pv((t+i)->P);
                             (t+j)->P=pl((t+j)->P);
+                            (t+i)->points++;
                         }
                     }
                 }
             }
         }
+    }
+    bubble_sort(t,N);
+    for(i=0;i<8;i++){
+        printf("%s %d %d %d %d",(t+i)->name,(t+i)->points,(t+i)->active_score,(t+i)->passive_score,((t+i)->active_score)-((t+i)->passive_score));
+    }
+    printf("<==================================================>");
+    for(i=8;i<N;i++){
+        printf("%s %d %d %d %d",(t+i)->name,(t+i)->points,(t+i)->active_score,(t+i)->passive_score,(((t+i)->active_score)-((t+i)->passive_score)));
     }
     free(t);
     return 0;
@@ -126,8 +144,36 @@ int pv(int x){
     return x;
 }
 int pl(int y){
-    if(y>0){
+    if(y>0 && y<10){
         y--;
     }
+    if(y==10){
+        y-=2;
+    }
     return y;
+}
+void score(int *z,int *w){
+    //first the winner then the loser
+    *z+=2;
+    *w++;
+}
+void bubble_sort(team *x,int n){
+    team temp;
+    int i,j;
+    for(i=1;i<=n;i++){
+        for(j=n;j>i;j--){
+            if(x[j-1].points<x[j].points){
+                temp=x[j-1];
+                x[j-1]=x[j];
+                x[j]=temp;
+            }
+            if(x[j-1].points==x[j].points){
+                if(((x[j-1].active_score)-(x[j-1].passive_score))<((x[j].active_score)-(x[j].passive_score))){
+                    temp=x[j-1];
+                    x[j-1]=x[j];
+                    x[j]=temp;
+                }
+            }
+        }
+    }
 }
